accc8b762dc8fef939b35394b589bc61ed1544d6
james-database <117508350+james-database@users.noreply.github.com>
2024-11-05 20:06:00 -0700

    Integrated Updated UI
    - Added mui/icons-material

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, createContext } from 'react';
 import MyMap from './components/Map/MyMap';
 import { MarkerProvider, MarkerContext } from './context/MarkerContext.jsx';
 import Button from '@mui/material/Button';
@@ -8,8 +8,71 @@ import { FaBroadcastTower } from "react-icons/fa";
 import { TbChartCircles } from "react-icons/tb";
 import { TfiLayoutLineSolid } from "react-icons/tfi";
 import { FaBookBookmark } from "react-icons/fa6";
+import Drawer from './components/UI/Minivariantdrawer';
+import { FaMapMarkedAlt } from "react-icons/fa";
+import { List, ListItem, ListItemText, ListItemIcon, IconButton, Typography} from '@mui/material';
+
+export const AppContext = createContext();
+
+function getIcon(type) {
+  switch (type) {
+    case 'RFF':
+      return <FaBroadcastTower size={18} />;
+    case 'Signal':
+      return <BsBroadcast size={18} />;
+    case 'Boat':
+      return <RiShip2Line size={18} />;
+    default:
+      return null;
+  }
+};
+
+// Bookmark List component
+function BookmarkList({ bookmarks, setBookmarkPosition }) {
+  return (
+    <div style={{ overflowY: 'auto', height: 'calc(100vh - 200px)' }}> {/* Adjust height as needed */}
+      <div className="flex items-center gap-5 font-bold text-2xl p-1 mb-0 ml-5 underline">
+        <div>Bookmarks</div>
+        <FaBookBookmark size={34} />
+      </div>
+      <List>
+        {bookmarks.map((bookmark, index) => (
+          <ListItem
+            key={bookmark.id || index} // Ensure a unique key prop
+            button
+            onClick={() => setBookmarkPosition(bookmark.latlng)}
+          >
+            <ListItemIcon
+              sx={{ minWidth: 30 }} // Adjust the minWidth to decrease the gap
+            >
+              {getIcon(bookmark.type)}
+            </ListItemIcon>
+            <ListItemText
+              primary={
+                <Typography variant="body1" component="span">
+                  {bookmark.type}
+                </Typography>
+              }
+              secondary={
+                <Typography variant="body2" component="span" align="center" sx={{ width: '100%' }}>
+                  <br />{bookmark.description} <br />
+                  Lat: {bookmark.latlng.lat.toFixed(2)}, Lng: {bookmark.latlng.lng.toFixed(2)}
+                </Typography>
+              }
+              sx={{ marginLeft: 0.5 }} // Adjust the margin to make the text closer to the icon
+            />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+}
+
+export { BookmarkList };
 
 function App() {
+
+  
   const { handleReplayClick, setPauseReplay, replay, pauseReplay, setReplay } = useContext(MarkerContext);
 
   const [bookmarks, setBookmarks] = useState([]);
@@ -25,46 +88,6 @@ function App() {
     areas: true,
   });
   const [decayRate, setDecayRate] = useState(30000);
-
-  function BookmarkList({ bookmarks }) {
-    return (
-        <div>
-          <div className="flex items-center gap-5 font-bold text-2xl p-1 mb-0 ml-5 underline">
-            <div>Bookmarks</div>
-            <FaBookBookmark size={34}/>
-          </div>
-          <div className="overflow-scroll h-80">
-            {bookmarks.map((bookmark, index) => (
-                <div className="mb-2 relative" key={index}>
-                  <Button className="w-full bg-black bg-opacity-75 text-white p-2 rounded-2xl" variant="contained" color="primary" onClick={() => setBookmarkPosition(bookmark.latlng)}>
-                    <div className="flex flex-col gap-1">
-                      <div className="flex items-center gap-6 font-bold text-xl p-1 ml-6">
-                        {getIcon(bookmark.type)}
-                        <h3 className="text-xl font-bold">{bookmark.type}</h3>
-                      </div>
-                      <p className="text-sm m-0">{bookmark.description}</p>
-                      <div className="text-xs text-white">
-                        <span>Lat: {bookmark.latlng.lat.toFixed(2)}</span>
-                        <span className='font-bold mx-1'> | </span>
-                        <span>Lng: {bookmark.latlng.lng.toFixed(2)}</span>
-                      </div>
-                    </div>
-                  </Button>
-                </div>
-            ))}
-          </div>
-        </div>
-    );
-  }
-
-  function getIcon(type) {
-    switch(type) {
-      case 'RFF': return <FaBroadcastTower size={18} />;
-      case 'Signal': return <BsBroadcast size={18} />;
-      case 'Boat': return <RiShip2Line size={18} />;
-      default: return null;
-    }
-  }
 
   function addBookmark(marker) {
     setBookmarks((prevBookmarks) => [...prevBookmarks, marker]);
@@ -106,67 +129,36 @@ function App() {
     );
   }
 
+  function decimalToDegrees(lat, long) {
+    let latDirection = "N";
+    let longDirection = "E";
+    if (lat < 0) {
+      latDirection = "S";
+      lat *= -1;
+    }
+    if (long < 0) {
+      longDirection = "W";
+      long *= -1;
+    }
+    const latdeg = Math.trunc(lat);
+    const latmin = Math.trunc((lat - latdeg) * 60);
+    const latsec = Math.trunc(((lat - latdeg) * 60 - latmin) * 60);
+
+    const longdeg = Math.trunc(long);
+    const longmin = Math.trunc((long - longdeg) * 60);
+    const longsec = Math.trunc(((long - longdeg) * 60 - longmin) * 60);
+
+    const latString = `${latdeg}\u00B0 ${latmin < 10 ? `0${latmin}` : latmin}' ${latsec < 10 ? `0${latsec}` : latsec}"${latDirection}`;
+    const longString = `${longdeg}\u00B0 ${longmin < 10 ? `0${longmin}` : longmin}' ${longsec < 10 ? `0${longsec}` : longsec}"${longDirection}`;
+
+    return `${latString} ${longString}`;
+  }
+
   return (
+    <AppContext.Provider value={{ handleInteractionModeChange, toggleVisibility, visibility, bookmarks, setBookmarkPosition }}>
       <MarkerProvider>
-        <div className="flex flex-row bg-gray-300">
-          <div className="flex flex-col w-96 h-full max-h-screen gap-2 py-4 px-4 bg-gray-300">
-            <ModeButton mode="dragging">
-              <RiDraggable /> Dragging
-            </ModeButton>
-            <ModeButton mode="RFF">
-              <FaBroadcastTower /> Add RFF Marker
-            </ModeButton>
-            <ModeButton mode="Signal">
-              <BsBroadcast /> Add Signal Marker
-            </ModeButton>
-            <ModeButton mode="area">
-              <TfiLayoutLineSolid /> Area
-            </ModeButton>
-            <VisibilityButton layer="RFF">RFF Visibility</VisibilityButton>
-            <VisibilityButton layer="Signal">Signal Visibility</VisibilityButton>
-            <VisibilityButton layer="Boat">Boat Visibility</VisibilityButton>
-            <VisibilityButton layer="lines">Lines Visibility</VisibilityButton>
-            <VisibilityButton layer="areas">Areas Visibility</VisibilityButton>
-            <VisibilityButton layer="circles">Circles Visibility</VisibilityButton>
-
-            <div className="my-4">
-              <label className="block font-bold mb-2">Set Decay Rate (ms):</label>
-              <input
-                  type="number"
-                  value={decayRate}
-                  onChange={(e) => setDecayRate(parseInt(e.target.value, 10))}
-                  className="border rounded p-2 w-full"
-                  min="1000"
-              />
-            </div>
-
-            <Button
-                variant="contained"
-                color="primary"
-                onClick={handleReplayClick}
-                className="mb-4"
-            >
-              Replay All Lines
-            </Button>
-            <Button
-                variant="contained"
-                color="secondary"
-                onClick={() => setPauseReplay(!pauseReplay)}
-                disabled={!replay}
-            >
-              {pauseReplay ? "Resume Replay" : "Pause Replay"}
-            </Button>
-            <Button
-                variant="contained"
-                color="error"
-                onClick={() => setReplay(false)}
-                disabled={!replay}
-            >
-              Stop Replay
-            </Button>
-
-            <BookmarkList bookmarks={bookmarks} />
-          </div>
+        <div>
+    
 
           <MyMap
               currentInteractionMode={currentInteractionMode}
@@ -178,20 +170,32 @@ function App() {
               decayRate={decayRate}
           />
 
-          <div className="absolute top-5 left-1/2 bg-black bg-opacity-75 text-white font-bold shadow shadow-gray-600 text-xl px-4 py-2 rounded-lg text-center"
-               style={{ zIndex: 1000 }}
+          <Drawer></Drawer>
+
+          <div
+            className={"absolute bottom-0 right-0 bg-gray-300 bg-opacity-100 text-white font-bold  w-3/12 h-[3.5vh]"
+              + " shadow shadow-gray-600 text-xl text-center"}
+            style={{ zIndex: 1000,  }}
           >
-            <div className="flex justify-between">
-              <p>Lat:</p>
-              <p>{cursorPosition.lat.toFixed(5)}</p>
-            </div>
-            <div className="flex justify-between">
-              <p>Lon:</p>
-              <p>{cursorPosition.lng.toFixed(5)}</p>
+            {/*Attributions*/}
+            <div className="flex flex-row justify-between px-3">
+              <div className={"flex flex-row"}>
+                <div className="flex flex-row">
+                  <a className="hover:underline text-black" href="https://leafletjs.com/">Leaflet</a>
+                  <p className="text-black">&nbsp;|&nbsp;</p>
+                  <a className="hover:underline text-black" href="www.esri.com">Esri</a>
+                </div>
+              </div>
+              {/*Lat-Long*/}
+              <div className={"flex flex-row"} style={{ marginLeft: '20px' }}> {/* Add margin here */}
+                <p className="text-black">{decimalToDegrees(cursorPosition.lat, cursorPosition.lng)}</p>
+              </div>
             </div>
           </div>
+
         </div>
       </MarkerProvider>
+    </AppContext.Provider>
   );
 }
 
